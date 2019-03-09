0505195b7031d78bc1419c9315ac7cc8603f0e23
Josh Pollock <josh@calderawp.com>
2019-03-09 12:02:55 -0500

    Paste in a checkbox field

--- src/index.js
+++ src/index.js
@@ -9,6 +9,33 @@ const TXT_FIELD = `
 	<input    type="text" data-field="fld_3023758" class=" form-control" id="${fieldId}" name="fld_3023758" value="" data-type="text"   aria-labelledby="fld_3023758Label" >			</div>
 </div>`;
 
+const CHECKBOX_FIELD = `<div data-field-wrapper="fld_8317391" class="form-group" id="fld_8317391_1-wrap">
+	<label id="fld_8317391Label" for="fld_8317391_1" class="control-label">Checkbox</label>
+	<div class="">
+											<div class="checkbox">
+				
+                <label for="fld_8317391_1_opt1519463">
+                            <input type="checkbox" data-label="a" data-field="fld_8317391" id="fld_8317391_1_opt1519463" class="fld_8317391_1" name="fld_8317391[opt1519463]" value="a" data-type="checkbox" data-checkbox-field="fld_8317391_1" data-calc-value="1">
+                    a                </label>
+
+                					</div>
+																	<div class="checkbox">
+				
+                <label for="fld_8317391_1_opt1595081">
+                            <input type="checkbox" data-label="b" data-field="fld_8317391" id="fld_8317391_1_opt1595081" class="fld_8317391_1" name="fld_8317391[opt1595081]" value="b" data-type="checkbox" data-checkbox-field="fld_8317391_1" data-calc-value="2">
+                    b                </label>
+
+                					</div>
+																	<div class="checkbox">
+				
+                <label for="fld_8317391_1_opt1319483">
+                            <input type="checkbox" data-label="c" data-field="fld_8317391" id="fld_8317391_1_opt1319483" class="fld_8317391_1" name="fld_8317391[opt1319483]" value="c" data-type="checkbox" data-checkbox-field="fld_8317391_1" data-calc-value="c">
+                    c                </label>
+
+                					</div>
+											</div>
+</div>`;
+
 function App() {
 	const [text, setText] = useState('Default Text');
 	const cfField = useRef(null);
